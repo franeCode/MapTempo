3be5fd76c3bd5158e96bf626d2144e50f44ae5be
Ana Popovic <franjetina@gmail.com>
2023-02-26 11:40:20 +0100

    code cleanup

--- scripts/app.js
+++ scripts/app.js
@@ -105,17 +105,21 @@ function updateTemperatureDisplay(unit) {
 }
 
 // Leaflet map
-const map = L.map('map').setView([0, 0], 2);
+const map = L.map("map").setView([0, 0], 2);
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-  // maxZoom: 5,
-  noWrap: true,
+L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    // maxZoom: 5,
+    noWrap: true,
     bounds: [
       [-90, -180],
       [90, 180],
     ],
-  attribution: '© <a href="https://www.arcgis.com/home/item.html?id=10df2279f9684e4a9f6a7f08febac2a9">Esri</a>'
-}).addTo(map);
+    attribution:
+      '© <a href="https://www.arcgis.com/home/item.html?id=10df2279f9684e4a9f6a7f08febac2a9">Esri</a>',
+  }
+).addTo(map);
 
 const resizeObserver = new ResizeObserver(() => {
   map.invalidateSize();
